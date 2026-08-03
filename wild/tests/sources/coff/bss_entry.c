52d18bba437e970b5b08fd459e5b61682003d519
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2026-08-03 03:37:41 +0300

    test: format PE smoke fixtures

--- wild/tests/sources/coff/bss_entry.c
+++ wild/tests/sources/coff/bss_entry.c
@@ -1,6 +1,6 @@
 extern volatile int zero_initialized;
 
 int mainCRTStartup(void) {
-    zero_initialized += 53;
-    return zero_initialized;
+  zero_initialized += 53;
+  return zero_initialized;
 }
