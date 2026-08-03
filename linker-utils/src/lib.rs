dcb605061e299ba20e0b191c55a1855a5948c994
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2026-08-03 04:05:38 +0300

    Add MSVC CRT runtime semantics

--- linker-utils/src/lib.rs
+++ linker-utils/src/lib.rs
@@ -5,6 +5,7 @@ pub mod coff_archives;
 pub mod coff_def;
 pub mod coff_import_library_writer;
 pub mod coff_imports;
+pub mod coff_runtime;
 pub mod coff_symbols;
 pub mod elf;
 pub mod loongarch64;
